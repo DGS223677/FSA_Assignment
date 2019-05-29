b74668b22e411da44cc6d83b8ad372369daf8dae
Sahoo, Dolagobinda (Cognizant) <223677@cognizant.com>
2019-05-29 13:59:18 -0700

    Commiting Final documents and Nbench report

--- ServerCode/ProjectManager/PerformanceTests/TaskPerfTests.cs
+++ ServerCode/ProjectManager/PerformanceTests/TaskPerfTests.cs
@@ -4,12 +4,16 @@ using ProjectManager.Controllers;
 
 namespace PerformanceTests
 {
-    [TestClass]
+   // [TestClass]
     public class TaskPerfTests
     {
-        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput,
+        [PerfBenchmark(NumberOfIterations = 3, RunMode = RunMode.Throughput,
         TestMode = TestMode.Test, SkipWarmups = true)]
         [ElapsedTimeAssertion(MaxTimeMilliseconds = 5000)]
+        [CounterThroughputAssertion("TestCounter", MustBe.GreaterThanOrEqualTo, 0.0d)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.GreaterThanOrEqualTo, ByteConstants.SixtyFourKb)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.LessThanOrEqualTo, 0.0d)]
+        [PerfSetup]
         public void PerformanceTests()
         {
             // Set up Prerequisites   
